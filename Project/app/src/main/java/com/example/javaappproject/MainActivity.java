de4f964edd21869282e1c9fc47357bd4d1a4b558
Hoseong-Ryu <52982845+Hoseong-Ryu@users.noreply.github.com>
2020-06-30 12:01:03 +0900

    ConnectedMap

--- Project/app/src/main/java/com/example/javaappproject/MainActivity.java
+++ Project/app/src/main/java/com/example/javaappproject/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.javaappproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button mapButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mapButton = findViewById(R.id.mapButton);
+        mapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),MapsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
